ef07131f19b856bd846904bf9e5c3365f2ecf16a
Louise Dennevi <louisedennevi@Louises-MacBook-Pro.local>
2020-09-01 20:37:04 +0200

    scss changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,30 @@ import Guest from "./component/guest/guestData"
 import BookingData from './component/booking/bookingData';
 import BookingSystem from './component/bookingsystem/bookingSystem';
 import GuestData from './component/guest/guestData';
+import '../src/App.scss';
+
+import desktopImage from './assets/background.jpg';
+import mobileImage from './assets/mobile-background.jpg';
 
 
 function App() {
 
 
-  
+  const imageUrl = window.innerWidth >= 650 ? desktopImage : mobileImage;
+
 
   return (
-    <div className="App">
+
+    <div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
+      <div className="App-content">
 
     <BookingSystem></BookingSystem>
-    
 
+    <h1 id="header">GREEN QUEEN</h1>
+    
+      </div>
     </div>
+    
   );
 }
 
