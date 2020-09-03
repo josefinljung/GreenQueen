a153300fe4d7dbd0ef6687d191c382fcd4e5759e
[teoreo] <[emma.teo@medieinstitutet.se]>
2020-09-03 14:24:17 +0200

    Route with switch

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,57 @@ import BookingData from './component/booking/bookingData';
 import BookingSystem from './component/bookingsystem/bookingSystem';
 import GuestData from './component/guest/guestData';
 import '../src/App.scss';
-
 import desktopImage from './assets/background.jpg';
 import mobileImage from './assets/mobile-background.jpg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './component/about/about';
+import Admin from './component/admin/admin';
+import Error from './component/error/error';
 
-
-function App() {
+ function App() {
 
 
   const imageUrl = window.innerWidth >= 650 ? desktopImage : mobileImage;
 
 
   return (
-
     <div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
       <div className="App-content">
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route exact path="/">
+              <BookingSystem />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+        </Router>
 
-  <BookingSystem></BookingSystem>
 
     <h1 id="header">GREEN QUEEN</h1>
     
