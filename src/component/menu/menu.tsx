7decd7bc67456ad6f2be2cbee6d00c0367cf3aee
[teoreo] <[emma.teo@medieinstitutet.se]>
2020-09-09 10:31:22 +0200

    menu-container html

--- src/component/menu/menu.tsx
+++ src/component/menu/menu.tsx
@@ -2,31 +2,35 @@ import React from 'react';
 
 export default function Menu (){
     return(
-    <div>
-        Spicy corn and chick pea burger (Vegansk)
-        Vegansk hamburgare av majs och kikärtor på glutenfritt bröd med tomat, saltgurka, och syrad rödlök. Serveras med coleslaw, sallad, tomatdressing och tortillachips.
-        BBQ marinated pulled Vegme burger (Vegansk)
-        Vegansk hamburgare meed BBQ-marinerad pulled Vegme på glutenfritt bröd med tomat, saltgurka, rostad lök och syrad rödlök.
-        Serveras med coleslaw, sallad, tomatdressing och tortillachips (GMO Free soy protein)
-        VEGAN ”SLOPPY JOE” (Vegansk)
-        Mexikansk kryddig chilifärsröra i pitabröd med tomat, picklad jalapeno, rostad lök, koriander, tortillachips and tomatsalsa.
-        (GMO Free soy protein)
-        VEGAN KEBAB IN PITA (Vegansk)
-
-        Kryddmarinerad Vegme, sallad, syrad rödlök, tomatsalsa, vitlökssås och syrad kål.
-        (GMO Free soy protein)
-        HALLOUMI-QUESADILLA
-        Grillad glutenfri tortilla med halloumi, oliver, chili, hummus, och sallad med tomatdressing.
-        SPICY FETA CHEESE QUESADILLA
-        Grillad glutenfri tortilla med fårost, fermenterad chili, rostad sötpotatis, hummus och sallad med tomatderssing
-        VEGAN QUESADILLA (Vegansk)
-        Grillad glutenfri tortilla med sötpotatis, paprika, oliver, picklad jalapeno, hummus och sallad med tomatdressing.
-        GRILLED BREAD SMASHED AVOCADO (Vegansk)
-        Grillat bröd med avokadoröra, syrad lök, sallad och chilisås.
-        Samt liten sallad med tomatsalsa
-        GRILLED BREAD MUSHROOM, ONIONS and TRUFFLE  OIL (Vegansk)
-        Grillat bröd med stekt svamp och lök, tryffelolja,
-        Samt liten sallad med tomatsalsa.
+        <React.Fragment>
+    <div className="menu-container">
+      
+        <h4 id="menu-h4">Spicy corn and chick pea burger (Vegansk) </h4> 
+            <p>Vegansk hamburgare av majs och kikärtor på glutenfritt bröd med tomat, saltgurka, och syrad rödlök.
+            Serveras med coleslaw, sallad, tomatdressing och tortillachips.</p>
+        <h4 id="menu-h4">BBQ marinated pulled Vegme burger (Vegansk)</h4>
+            <p>Vegansk hamburgare meed BBQ-marinerad pulled Vegme på glutenfritt bröd med tomat, saltgurka, rostad lök och syrad rödlök.
+            Serveras med coleslaw, sallad, tomatdressing och tortillachips (GMO Free soy protein)</p>
+        <h4 id="menu-h4"> VEGAN ”SLOPPY JOE” (Vegansk)</h4>
+            <p>Mexikansk kryddig chilifärsröra i pitabröd med tomat, picklad jalapeno, rostad lök, koriander, tortillachips and tomatsalsa.
+            (GMO Free soy protein)</p>
+        <h4 id="menu-h4">VEGAN KEBAB IN PITA (Vegansk)</h4>
+            <p>Kryddmarinerad Vegme, sallad, syrad rödlök, tomatsalsa, vitlökssås och syrad kål.
+            (GMO Free soy protein)</p>
+        <h4 id="menu-h4">HALLOUMI-QUESADILLA</h4>
+            <p>Grillad glutenfri tortilla med halloumi, oliver, chili, hummus, och sallad med tomatdressing.</p>
+        <h4 id="menu-h4">SPICY FETA CHEESE QUESADILLA</h4>
+            <p>Grillad glutenfri tortilla med fårost, fermenterad chili, rostad sötpotatis, hummus och sallad med tomatderssing.</p>
+        <h4 id="menu-h4">VEGAN QUESADILLA (Vegansk)</h4>
+            <p>Grillad glutenfri tortilla med sötpotatis, paprika, oliver, picklad jalapeno, hummus och
+            sallad med tomatdressing.</p>
+        <h4 id="menu-h4">GRILLED BREAD SMASHED AVOCADO (Vegansk)</h4>
+            <p>Grillat bröd med avokadoröra, syrad lök, sallad och chilisås.
+            Samt liten sallad med tomatsalsa.</p>
+        <h4 id="menu-h4">GRILLED BREAD MUSHROOM, ONIONS and TRUFFLE  OIL (Vegansk)</h4>
+            <p>Grillat bröd med stekt svamp och lök, tryffelolja,
+            Samt liten sallad med tomatsalsa.</p>
     </div>
+    </React.Fragment>
     )
 }
