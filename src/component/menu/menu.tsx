40c0d7cb04afca7ca98a2c32bbe5f436df465de9
[teoreo] <[emma.teo@medieinstitutet.se]>
2020-09-09 10:46:51 +0200

    fixed menu page error

--- src/component/menu/menu.tsx
+++ src/component/menu/menu.tsx
@@ -6,32 +6,6 @@ export default function Menu (){
 
         <React.Fragment>
             <img src={BounceImage} id="startpage"/>
-        <div>
-        Spicy corn and chick pea burger (Vegansk)
-        Vegansk hamburgare av majs och kikärtor på glutenfritt bröd med tomat, saltgurka, och syrad rödlök. Serveras med coleslaw, sallad, tomatdressing och tortillachips.
-        BBQ marinated pulled Vegme burger (Vegansk)
-        Vegansk hamburgare meed BBQ-marinerad pulled Vegme på glutenfritt bröd med tomat, saltgurka, rostad lök och syrad rödlök.
-        Serveras med coleslaw, sallad, tomatdressing och tortillachips (GMO Free soy protein)
-        VEGAN ”SLOPPY JOE” (Vegansk)
-        Mexikansk kryddig chilifärsröra i pitabröd med tomat, picklad jalapeno, rostad lök, koriander, tortillachips and tomatsalsa.
-        (GMO Free soy protein)
-        VEGAN KEBAB IN PITA (Vegansk)
-
-        Kryddmarinerad Vegme, sallad, syrad rödlök, tomatsalsa, vitlökssås och syrad kål.
-        (GMO Free soy protein)
-        HALLOUMI-QUESADILLA
-        Grillad glutenfri tortilla med halloumi, oliver, chili, hummus, och sallad med tomatdressing.
-        SPICY FETA CHEESE QUESADILLA
-        Grillad glutenfri tortilla med fårost, fermenterad chili, rostad sötpotatis, hummus och sallad med tomatderssing
-        VEGAN QUESADILLA (Vegansk)
-        Grillad glutenfri tortilla med sötpotatis, paprika, oliver, picklad jalapeno, hummus och sallad med tomatdressing.
-        GRILLED BREAD SMASHED AVOCADO (Vegansk)
-        Grillat bröd med avokadoröra, syrad lök, sallad och chilisås.
-        Samt liten sallad med tomatsalsa
-        GRILLED BREAD MUSHROOM, ONIONS and TRUFFLE  OIL (Vegansk)
-        Grillat bröd med stekt svamp och lök, tryffelolja,
-        Samt liten sallad med tomatsalsa.
-        <React.Fragment>
     <div className="menu-container">
       
         <h4 id="menu-h4">Spicy corn and chick pea burger (Vegansk) </h4> 
