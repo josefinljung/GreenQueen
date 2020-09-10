a9d328f4d8f7c055f491fbf6fc3a64a0a93d3230
Louise Dennevi <louisedennevi@Louises-MacBook-Pro.local>
2020-09-10 11:05:24 +0200

    error page styling 404 not found

--- src/component/error/error.tsx
+++ src/component/error/error.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import {BrowserRouter as Router, Link} from "react-router-dom";
+import BounceImage from '../../assets/bounce-image.png';
 
 export default function Error(){
+    
     return(          
+
+        <React.Fragment>
+            <img src={BounceImage} id="startpage"/>  
             <div className="errorContainer">
+                <h1 className="error-header">404 NOT FOUND</h1>
                 <p className="errorText">
                 Oh no! The page you were looking for doesn't seem to exist.
                 Perhaps you wanted to make a <Link to="/">reservation</Link>?
-                </p>  
+                </p>
             </div>
+        </React.Fragment>
     )
 }
